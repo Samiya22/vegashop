8658ad85b13ec0689fe1d543fbec9beff5ac3707
samiya22 <samiya.sultanova21@gmail.com>
2022-05-10 12:31:43 +0500

    xatolik togirlandi

--- vegashop/wsgi.py
+++ vegashop/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vegashop.settings'
 
 application = get_wsgi_application()
